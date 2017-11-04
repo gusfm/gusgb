28cb2290b95d2caf81787fad2788a14c63804874
Gustavo Foletto Mora <gusfmora@gmail.com>
2017-11-04 17:47:58 -0200

    Fix TIMA behavior
    - TIMA should be reloaded and the interrupt flag set only after one
    cycle has passed since the overflow
    - During that time it should contain the zero value
    - Prevent reload if writing to TIMA during the overflow cycle
    - Ignore writes to TIMA if it was just reloaded
    - If TMA is written in the same cycle that TIMA is reloaded, write the
    value to TIMA as well

--- src/timer.c
+++ src/timer.c
@@ -11,11 +11,19 @@ typedef struct {
     unsigned int speed;
 } gb_clock_t;
 
+/* TIMA states. */
+typedef enum {
+    TIMA_STATE_COUNTING = 0, /* Normal operation */
+    TIMA_STATE_OVERFLOW,     /* Overflow happened. */
+    TIMA_STATE_RELOADING,    /* TIMA is being reloaded. */
+} tima_state_t;
+
 uint8_t div_;             /* [$ff04] Divider Register (R/W) */
 uint8_t tima_;            /* [$ff05] Timer counter (R/W) */
 uint8_t tma_;             /* [$ff06] Timer Modulo (R/W) */
 uint8_t tac_;             /* [$ff07] Timer Control (R/W) */
 gb_clock_t clock_;        /* Internal timer state */
+tima_state_t tima_state_; /* TIMA operation states. */
 static unsigned int clocks[] = {1024, 16, 64, 256};
 
 void timer_init(void)
@@ -28,6 +36,7 @@ void timer_init(void)
     clock_.internal = 0;
     clock_.speed = 1;
     clock_.select = clocks[0];
+    tima_state_ = TIMA_STATE_COUNTING;
 }
 
 void timer_step(uint32_t clock_step)
@@ -36,13 +45,27 @@ void timer_step(uint32_t clock_step)
     clock_.internal += clock_step;
     /* Check whether a step needs to be made in the timer. */
     if (tac_ & TIMER_ENABLE) {
-        while (clock_.main >= clock_.select) {
-            clock_.main -= clock_.select;
-            if (tima_ == 0xff) {
-                /* If TIMA overflows, load it with TMA and generate interrupt.
+        switch (tima_state_) {
+            case TIMA_STATE_COUNTING:
+                break;
+            case TIMA_STATE_OVERFLOW:
+                /* After one cycle, TIMA should be reloaded and the interrupt
+                 * flag set.
                  */
                 tima_ = tma_;
                 interrupt_raise(INTERRUPTS_TIMER);
+                tima_state_ = TIMA_STATE_RELOADING;
+                break;
+            case TIMA_STATE_RELOADING:
+                tima_state_ = TIMA_STATE_COUNTING;
+                break;
+        }
+        while (clock_.main >= clock_.select) {
+            clock_.main -= clock_.select;
+            if (tima_ == 0xff) {
+                /* When TIMA overflows, it contains zero for 1 cycle. */
+                tima_ = 0;
+                tima_state_ = TIMA_STATE_OVERFLOW;
             } else {
                 ++tima_;
             }
@@ -71,6 +94,7 @@ void timer_write_byte(uint16_t addr, uint8_t val)
 {
     switch (addr) {
         case 0xFF04:
+            /* Increase tima when writing to div triggers a bit flip. */
             switch (tac_ & 3) {
                 case 0:
                     /* 4096 Hz mode bit 9 change from 1 to 0. */
@@ -99,10 +123,28 @@ void timer_write_byte(uint16_t addr, uint8_t val)
             clock_.main = 0;
             break;
         case 0xFF05:
-            tima_ = val;
+            switch (tima_state_) {
+                case TIMA_STATE_COUNTING:
+                    /* Normal operation. */
+                    tima_ = val;
+                    break;
+                case TIMA_STATE_OVERFLOW:
+                    /* Prevent reload if writing to it during overflow cycle. */
+                    tima_ = val;
+                    tima_state_ = TIMA_STATE_COUNTING;
+                    break;
+                case TIMA_STATE_RELOADING:
+                    /* Ignore writes if TIMA was just reloaded. */
+                    break;
+            }
             break;
         case 0xFF06:
             tma_ = val;
+            if (tima_state_ == TIMA_STATE_RELOADING) {
+                /* If TMA is written in the same cycle that TIMA is reloaded,
+                 * write the value to TIMA as well. */
+                tima_ = tma_;
+            }
             break;
         case 0xFF07:
             clock_.select = clocks[val & 3] * clock_.speed;
