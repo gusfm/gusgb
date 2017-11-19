90bea530f73651969d58c46e612f497dc98a9185
Gustavo Foletto Mora <gusfmora@gmail.com>
2017-11-19 10:52:44 -0200

    Fix rom test script

--- test/test_roms.sh
+++ test/test_roms.sh
@@ -3,11 +3,12 @@ if [ "$#" -ne 1 ]; then
     echo "Syntax: $0 <test path>"
     exit
 fi
-FILES=$(find $1 -type f -name "*.gb")
 OUT_FILE=results-$(git rev-parse --short=8 HEAD).txt
-for file in $FILES
+IFS=$'\n'
+for file in $(find $1 -type f -name "*.gb")
 do
-    ./gusgb $file
+    echo "Testing $file"
+    ./gusgb "$file"
     echo "Type the result [ OK | NOK ] and hit ENTER"
     read result
     echo "$file $result" >> "$OUT_FILE"
