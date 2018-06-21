d94860d8b83db783a8a70976be5cf1b78bb47679
Gustavo Foletto Mora <gusfmora@gmail.com>
2018-06-20 21:58:06 -0300

    Fix wave channel's frequency timer period

--- src/apu/wave_ch.c
+++ src/apu/wave_ch.c
@@ -13,7 +13,7 @@ void wave_ch_tick(wave_ch_t *c, unsigned int clock_step)
 {
     c->timer -= clock_step;
     if (c->timer <= 0) {
-        c->timer += (2048 - c->frequency) * 4;
+        c->timer += (2048 - c->frequency) * 2;
         c->position = (c->position + 1) & 0x1f;
         if (c->volume) {
             uint8_t out = c->wave_ram[c->position / 2];
@@ -87,7 +87,7 @@ uint8_t wave_ch_read_reg3(wave_ch_t *c)
 void wave_ch_write_reg3(wave_ch_t *c, uint8_t val)
 {
     c->frequency = (c->frequency & 0x700) | val;
-    c->timer = (2048 - c->frequency) * 4;
+    c->timer = (2048 - c->frequency) * 2;
 }
 
 uint8_t wave_ch_read_reg4(wave_ch_t *c)
@@ -99,7 +99,7 @@ void wave_ch_write_reg4(wave_ch_t *c, uint8_t val)
 {
     bool old_length_en = c->length.enabled;
     c->frequency = ((val & 0x7) << 8) | (c->frequency & 0xff);
-    c->timer = (2048 - c->frequency) * 4;
+    c->timer = (2048 - c->frequency) * 2;
     c->length.enabled = val & 0x40;
     if (!old_length_en && frame_sequencer.out_clock & 1) {
         wave_ch_length_counter(c);
