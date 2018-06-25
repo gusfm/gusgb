52fbe8ea5f51237155613366341ec2fddd346690
Gustavo Foletto Mora <gusfmora@gmail.com>
2018-06-25 19:12:59 -0300

    Fix MBC5 implementation

--- src/cartridge/mbc5.c
+++ src/cartridge/mbc5.c
@@ -1,64 +1,50 @@
 #include "mbc5.h"
 #include "cart.h"
 
-typedef struct {
-    uint8_t rom_bank;
-    uint8_t ram_bank;
-    uint8_t mode;
-} mbc_t;
-
-mbc_t MBC;
+static uint32_t rom_bank;
+static uint32_t ram_bank;
 extern cart_t CART;
 
 void mbc5_init(void)
 {
-    MBC.rom_bank = 1;
-    MBC.ram_bank = 0;
-    MBC.mode = 0;
-}
-
-static void mbc5_change_bank(void)
-{
-    unsigned int rom_bank_tmp = MBC.rom_bank % CART.rom.max_bank;
-    CART.rom.offset = (unsigned int)(rom_bank_tmp) * 0x4000;
+    rom_bank = 0;
+    ram_bank = 0;
+    CART.rom.offset = 0;
 }
 
 void mbc5_write(uint16_t addr, uint8_t val)
 {
-    if (addr <= 0x1fff) {
-        /* Enable/disable external RAM. */
-        CART.ram.enabled = (val & 0x0f) == 0x0a ? true : false;
-    } else if (addr <= 0x3fff) {
-        /* Switch between banks 1-31 (value 0 is seen as 1). */
-        uint8_t bankl = val & 0x1f;
-        if (bankl == 0)
-            bankl = 1;
-        if (MBC.mode == 0) {
-            MBC.rom_bank = (uint8_t)((MBC.rom_bank & 0x60) | bankl);
-        } else {
-            MBC.rom_bank = bankl;
-        }
-        mbc5_change_bank();
-    } else if (addr <= 0x5fff) {
-        if (MBC.mode) {
-            /* RAM mode: switch RAM bank 0-3. */
-            MBC.ram_bank = val & 3;
-            CART.ram.offset = (unsigned int)(MBC.ram_bank % CART.ram.max_bank) * 0x2000;
-        } else {
-            /* ROM mode (high 2 bits): switch ROM bank "set" {1-31}-{97-127}. */
-            MBC.rom_bank =
-                (uint8_t)((MBC.rom_bank & 0x1f) | ((val & 3) << 5));
-            mbc5_change_bank();
-        }
-    } else {
-        MBC.mode = val & 1;
+    switch ((addr >> 12) & 7) {
+        case 0:
+        case 1:
+            /* 0x0000 - 0x1fff: RAM enable */
+            CART.ram.enabled = (val & 0x0f) == 0x0a;
+            break;
+        case 2:
+            /* 0x2000 - 0x2fff: Low bits of ROM bank number */
+            rom_bank = (rom_bank & 0x100) | val;
+            CART.rom.offset = (rom_bank % CART.rom.max_bank) << 14;
+            break;
+        case 3:
+            /* 0x3000 - 0x3fff: High bit of ROM bank number */
+            rom_bank = ((uint16_t)(val & 1) << 8) | (rom_bank & 0xff);
+            CART.rom.offset = (rom_bank % CART.rom.max_bank) << 14;
+            break;
+        case 4:
+        case 5:
+            /* 0x4000 - 0x5fff: RAM bank number */
+            ram_bank = val & 0x0f;
+            CART.ram.offset = (ram_bank % CART.ram.max_bank) << 13;
+            break;
+        default:
+            printf("%s: invalid address: 0x%04x\n", __func__, addr);
+            break;
     }
 }
 
 uint8_t mbc5_ram_read(uint16_t addr)
 {
-    size_t pos = CART.ram.offset + (addr & 0x1fff);
-    if (CART.ram.enabled && pos < CART.ram.size) {
+    if (CART.ram.enabled) {
         return CART.ram.bytes[CART.ram.offset + (addr & 0x1fff)];
     } else {
         return 0xff;
@@ -68,9 +54,6 @@ uint8_t mbc5_ram_read(uint16_t addr)
 void mbc5_ram_write(uint16_t addr, uint8_t val)
 {
     if (CART.ram.enabled) {
-        size_t pos = CART.ram.offset + (addr & 0x1fff);
-        if (pos < CART.ram.size) {
-            CART.ram.bytes[pos] = val;
-        }
+        CART.ram.bytes[CART.ram.offset + (addr & 0x1fff)] = val;
     }
 }
