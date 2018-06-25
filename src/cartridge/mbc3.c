22c7d398287c7159a73980af310d179c12021f02
Gustavo Foletto Mora <gusfmora@gmail.com>
2018-06-25 20:41:28 -0300

    Improve MBC3 code

--- src/cartridge/mbc3.c
+++ src/cartridge/mbc3.c
@@ -6,55 +6,46 @@
 #define HOUR_SECS (60 * 60)
 #define DAY_SECS (60 * 60 * 24)
 
-typedef struct {
-    uint8_t rom_bank;
-    uint8_t ram_bank;
-    rtc_t rtc;
-} mbc_t;
+static uint32_t rom_bank;
+static uint32_t ram_bank;
+static rtc_t rtc;
 
-mbc_t MBC;
 extern cart_t CART;
 
 void mbc3_init(void)
 {
-    MBC.rom_bank = 1;
-    MBC.ram_bank = 0;
+    rom_bank = 1;
+    ram_bank = 0;
 }
 
 int mbc3_rtc_load(FILE *file)
 {
     if (file) {
-        int rv = fread(&MBC.rtc, 1, sizeof(MBC.rtc), file);
-        if (rv != sizeof(MBC.rtc)) {
+        int rv = fread(&rtc, 1, sizeof(rtc), file);
+        if (rv != sizeof(rtc)) {
             fprintf(stderr, "RTC not present in save file\n");
             return -1;
         }
     } else {
-        memset(&MBC.rtc.time, 0, sizeof(MBC.rtc.time));
-        memset(&MBC.rtc.latched_time, 0, sizeof(MBC.rtc.latched_time));
-        MBC.rtc.time_start = time(NULL);
+        memset(&rtc.time, 0, sizeof(rtc.time));
+        memset(&rtc.latched_time, 0, sizeof(rtc.latched_time));
+        rtc.time_start = time(NULL);
     }
     printf("RTC current: ");
-    rtc_print(&MBC.rtc.time);
+    rtc_print(&rtc.time);
     printf("RTC latched: ");
-    rtc_print(&MBC.rtc.latched_time);
+    rtc_print(&rtc.latched_time);
     return 0;
 }
 
 int mbc3_rtc_save(FILE *file)
 {
-    int rv = fwrite(&MBC.rtc, 1, sizeof(MBC.rtc), file);
-    if (rv != sizeof(MBC.rtc)) {
+    int rv = fwrite(&rtc, 1, sizeof(rtc), file);
+    if (rv != sizeof(rtc)) {
         fprintf(stderr, "Could not save RTC to save file\n");
         return -1;
     }
     return 0;
-}
-
-static void mbc3_change_bank(void)
-{
-    unsigned int rom_bank_tmp = MBC.rom_bank % CART.rom.max_bank;
-    CART.rom.offset = (unsigned int)(rom_bank_tmp)*0x4000;
 }
 
 void mbc3_rtc_update(rtc_time_t *time, time_t diff)
@@ -87,18 +78,18 @@ void mbc3_write(uint16_t addr, uint8_t val)
     } else if (addr <= 0x3fff) {
         /* Select ROM bank (value 0 is seen as 1). */
         uint8_t bank = val & 0x7f;
-        MBC.rom_bank = bank == 0 ? 1 : bank;
-        mbc3_change_bank();
+        rom_bank = bank == 0 ? 1 : bank;
+        CART.rom.offset = (rom_bank % CART.rom.max_bank) << 14;
     } else if (addr <= 0x5fff) {
         /* Select RAM bank. */
-        MBC.ram_bank = val;
+        ram_bank = val;
     } else {
         /* Latch Clock Data. */
         static uint8_t last = 0xff;
         if (last == 0 && val == 1) {
-            time_t time_diff = time(NULL) - MBC.rtc.time_start;
-            mbc3_rtc_update(&MBC.rtc.time, time_diff);
-            MBC.rtc.latched_time = MBC.rtc.time;
+            time_t time_diff = time(NULL) - rtc.time_start;
+            mbc3_rtc_update(&rtc.time, time_diff);
+            rtc.latched_time = rtc.time;
         }
         last = val;
     }
@@ -106,13 +97,12 @@ void mbc3_write(uint16_t addr, uint8_t val)
 
 uint8_t mbc3_ram_read(uint16_t addr)
 {
-    size_t pos = CART.ram.offset + (addr & 0x1fff);
-    if (CART.ram.enabled && pos < CART.ram.size) {
-        uint8_t bank = MBC.ram_bank;
+    if (CART.ram.enabled) {
+        uint8_t bank = ram_bank;
         if (bank <= 7) {
             return CART.ram.bytes[CART.ram.offset + (addr & 0x1fff)];
         } else if (bank <= 0x0c) {
-            return MBC.rtc.latched_time.reg[bank - 8];
+            return rtc.latched_time.reg[bank - 8];
         } else {
             return 0xff;
         }
@@ -123,18 +113,17 @@ uint8_t mbc3_ram_read(uint16_t addr)
 
 void mbc3_ram_write(uint16_t addr, uint8_t val)
 {
-    size_t pos = CART.ram.offset + (addr & 0x1fff);
-    if (CART.ram.enabled && pos < CART.ram.size) {
-        uint8_t bank = MBC.ram_bank;
+    if (CART.ram.enabled) {
+        uint8_t bank = ram_bank;
         if (bank <= 7) {
-            CART.ram.bytes[pos] = val;
+            CART.ram.bytes[CART.ram.offset + (addr & 0x1fff)] = val;
         } else if (bank <= 0x0c) {
-            if (MBC.rtc.time.reg[4] & 0x40 || (bank == 0x0c && val & 0x40)) {
+            if (rtc.time.reg[4] & 0x40 || (bank == 0x0c && val & 0x40)) {
                 /* The Halt Flag is supposed to be set before writing to the RTC
                  * registers. */
-                MBC.rtc.time.reg[bank - 8] = val;
+                rtc.time.reg[bank - 8] = val;
                 if ((val & 0x40) == 0) {
-                    MBC.rtc.time_start = time(NULL);
+                    rtc.time_start = time(NULL);
                 }
             }
         }
